ba310dbd11182178c95c924fc0e4c68c0589fb4d
BananaChiu <agimz0330@gmail.com>
2022-10-02 15:33:05 +0800

    Initial Commit

---
+++ HW1_00857051/HW1_00857051App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1_00857051App.swift
+//  HW1_00857051
+//
+//  Created by User23 on 2022/10/2.
+//
+
+import SwiftUI
+
+@main
+struct HW1_00857051App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
